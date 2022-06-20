8d0decf50cbaa24610c10bffaf40a256bc33310b
DongwooKim <kimphysicsman@gmail.com>
2022-06-20 10:30:24 +0900

    추가 - 게시글 작성 기능

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,14 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Article
+from .models import Article, Category
 from .serializers import ArticleSerializer
+from user.permissions import RegistedMoreThanAWeekUser
 
 # 사용자 게시글 기능
 class UserArticle(APIView):
-    # # 사용자의 게시글의 제목 리스트 보여주기
-    # def get(self, request):
-    #     user = request.user
-    #     if not user.is_authenticated:
-    #         msg = '로그인을 해주세요.'
-    #         return Response({'message': msg})
+    permission_classes = [RegistedMoreThanAWeekUser]
 
-    #     articles = Article.objects.filter(author=user)
-
-    #     titles = [ article.title for article in articles ]
-
-    #     return Response({'titles': titles})
-
-    # 사용자의 게시글 정보 보여주기
     def get(self, request):
         user = request.user
         if not user.is_authenticated:
@@ -29,3 +18,32 @@ class UserArticle(APIView):
         articles = Article.objects.filter(author=user)
 
         return Response(ArticleSerializer(articles, many=True).data)
+
+    # 게시글 작성 기능
+    def post(self, request):
+        title = request.data['title']
+        categorys = request.data['categorys']
+        content = request.data['content']
+
+        if len(title) <= 5:
+            msg = 'title이 5자 이하이면 게시글을 작성할 수 없습니다.'
+            return Response({'message': msg})
+
+        if len(content) <=20:
+            msg = 'content가 20자 이하이면 게시글을 작성할 수 없습니다.'
+            return Response({'message': msg})
+        
+        if not categorys or len(categorys) == 0:
+            msg = 'category를 지정해야합니다.'
+            return Response({'message': msg})
+        
+        categorys = [Category.objects.get(category_name=category) for category in categorys]
+    
+
+        user = request.user
+
+        article = Article.objects.create(author=user, title=title, content=content)
+        article.category.set(categorys)
+        article.save()
+
+        return Response({'message': '게시글 저장 완료!'})
