dd497ce52f0bbfa0f1b513a7782fad7b04ef8381
DongwooKim <kimphysicsman@gmail.com>
2022-06-21 20:46:12 +0900

    추가 - admin 페이지에서 thumbnail 보이기

--- product/models.py
+++ product/models.py
@@ -9,14 +9,7 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     show_start_at = models.DateTimeField(default="2022-06-20 00:00:00")
     show_end_at = models.DateTimeField(default="2022-06-25 00:00:00")
-    thumbnail = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+    thumbnail = models.ImageField(upload_to='product/img', height_field=None, width_field=None, max_length=100)
     
     def __str__(self):
         return f'Product : {self.title} - {self.author}'
-
-    def image_tag(self):
-        from django.utils.html import escape
-        return u'<img src="%s" />' % escape('https://www.notion.so/image/https%3A%2F%2Fpost-phinf.pstatic.net%2FMjAxOTA5MjZfNzEg%2FMDAxNTY5NDY2MzY5MzM0.FxfbYJ-W2f0YttSaWjQRg-tcApxpLWx1R8lccTCT4zgg.5Q4VvR0HY_KYUUuVMBf14sAk-eOS3RBqOJKfm29LNSEg.JPEG%2F%25EB%25AC%25BB%25EA%25B3%25A0_%25EB%2596%25A0%25EB%25B8%2594%25EB%259F%25AC_%25EA%25B0%2580.jpg%3Ftype%3Dw1200?table=block&id=c72ec1ae-8d9a-4f9f-ba34-7276e7d1839c&spaceId=18a55a3f-2515-4f20-9f7f-04187576573f&width=250&userId=f65f0c54-5dc8-4f1b-acf4-e4d6d5c93a82&cache=v2')
-    
-    image_tag.short_description = 'Image'
-    image_tag.allow_tags = True
